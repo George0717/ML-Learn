a12d698d2ca4429dc057d4fd883c0d7513d16fa7
George0717 <raden7george@gmail.com>
2023-12-07 16:04:55 +0700

    second_commit
    k_means,pca,svm,svr

---
+++ svm.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+df = pd.read_csv('diabetes.csv') # Untuk membaca file diabetes.csv (path file disesuaikan)
+
+# memisahkan atribut pada dataset dan menyimpannya pada sebuah variabel
+X = df[df.columns[:8]]
+ 
+# memisahkan label pada dataset dan menyimpannya pada sebuah variabel
+y = df['Outcome']
+
+# standarisasi nilai-nilai dari dataset
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+
+# memisahkan data untuk training dan testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+ 
+# membuat objek SVC dan memanggil fungsi fit untuk melatih model
+clf = SVC()
+clf.fit(X_train, y_train)
+
+print(clf.score(X_test, y_test))
